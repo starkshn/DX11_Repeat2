806d83ca593df1a8572af6f1041689c6e3711c1f
DESKTOP-6QQQTSE\김남영 <starkshn97@gmail.com>
2023-09-04 17:26:00 +0900

    [프로젝트 설정]

---
+++ GameCoding_Repeat2/Game.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Init(HWND hWnd)
+{
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
---
+++ GameCoding_Repeat2/GameCoding_Repeat2.cpp
@@ -0,0 +1,135 @@
+﻿// GameCoding_Repeat2.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GameCoding_Repeat2.h"
+#include "Game.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE       hInst;                                // 현재 인스턴스입니다.
+HWND            hWnd;
+const wchar_t*  className = L"GameCoding_Repeat2";
+const wchar_t*  TitleName = L"Client";
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    // 1) Window 생성
+    MyRegisterClass(hInstance);
+
+    // 2) 윈도우 창 생성
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    Game game;
+    game.Init(hWnd);
+    MSG msg = {};
+
+    while (msg.message != WM_QUIT)
+    {
+        if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            game.Update();
+            game.Render();
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMECODINGREPEAT2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(NULL);
+    wcex.lpszClassName  = className;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   RECT windowRect = {0, 0, GWinSizeX, GWinSizeY};
+
+   hWnd = CreateWindowW(className, TitleName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0,
+       windowRect.right - windowRect.left,
+       windowRect.bottom - windowRect.top,
+       nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_EXIT:
+            {
+                DestroyWindow(hWnd);
+            }
+            break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
